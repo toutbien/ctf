7a5c2741e26d1802a8e2e1e24c80256946e28d42
gr8goat <90510480+toutbien@users.noreply.github.com>
2024-07-22 19:00:18 -0400

    Create direnum.py
    Directory Enumeration Script 
    Removing 404 errors
    Writing back to terminal valid http directories

---
+++ direnum.py
@@ -0,0 +1,16 @@
+#enumeration script
+
+import requests 
+import sys 
+
+sub_list = open("wordlist_test.txt").read() 
+directories = sub_list.splitlines()
+
+for dir in directories:
+    dir_enum = f"http://{sys.argv[1]}/{dir}.html" 
+    r = requests.get(dir_enum)
+    if r.status_code==404: 
+        pass
+    else:
+        print("Valid directory:" ,dir_enum)
+
